14efaf991f8f37d43c28c96b46cae7fd39cf62e3
quarzasiphix <76133968+quarzasiphix@users.noreply.github.com>
2026-02-02 06:33:15 +0100

    latest

--- next.config.js
+++ next.config.js
@@ -1,21 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-    // Exclude blog directory from build
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'].map(ext => {
     return ext;
   }),
-  async redirects() {
-    return [
-      {
-        source: '/blog/:path*',
-        destination: '/',
-        permanent: false,
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
