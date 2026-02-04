7fde7e9f1f75588a30663731ba14cf5ba04bb424
quarzasiphix <76133968+quarzasiphix@users.noreply.github.com>
2026-02-04 04:30:20 +0100

    latest

--- app/robots.ts
+++ app/robots.ts
@@ -3,12 +3,14 @@ import { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://nekrolog-lodz.com'
+  
   return {
     rules: {
       userAgent: '*',
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://nekrolog-lodz.com/sitemap.xml',
+    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://nekrolog-lodz.com'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://nekrolog-lodz.com'
 
   return [
     {
